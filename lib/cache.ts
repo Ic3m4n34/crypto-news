847bd23299fecdeb332127e1bbdd2657fa1a691f
Nico Meyer <nicomeyer@hey.com>
2022-09-12 08:41:40 -0500

    feat: better caching key

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,17 +1,18 @@
 import redisClient from '@/lib/redis';
 
 const getOrSetCache = async (key: string, cb, ttlInSeconds?: number) => {
+  const keyWithDateTime = `${key}_${new Date().toISOString()}`;
   const ttl = ttlInSeconds || 60 * 60 * 24;
-  const cachedData = await redisClient.get(key);
+  const cachedData = await redisClient.get(keyWithDateTime);
 
   if (cachedData) return JSON.parse(cachedData);
 
   const data = await cb();
   try {
-    redisClient.set(key, JSON.stringify(data), 'EX', ttl);
+    redisClient.set(keyWithDateTime, JSON.stringify(data), 'EX', ttl);
     return data;
   } catch (error) {
-    console.error(`Error setting cache for key ${key}`, error);
+    console.error(`Error setting cache for key ${keyWithDateTime}`, error);
     return null;
   }
 };
