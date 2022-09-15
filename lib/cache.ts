b0a36ba8fbb656a3c58b3b52aaa339b1d0d91dc6
Nico Meyer <nicomeyer@hey.com>
2022-09-15 08:01:34 -0500

    fix: cache

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,18 +1,17 @@
 import redisClient from '@/lib/redis';
 
 const getOrSetCache = async (key: string, cb, ttlInSeconds?: number) => {
-  const keyWithDateTime = `${key}_${new Date().toISOString()}`;
   const ttl = ttlInSeconds || 60 * 60 * 24;
-  const cachedData = await redisClient.get(keyWithDateTime);
+  const cachedData = await redisClient.get(key);
 
   if (cachedData) return JSON.parse(cachedData);
 
   const data = await cb();
   try {
-    redisClient.set(keyWithDateTime, JSON.stringify(data), 'EX', ttl);
+    redisClient.set(key, JSON.stringify(data), 'EX', ttl);
     return data;
   } catch (error) {
-    console.error(`Error setting cache for key ${keyWithDateTime}`, error);
+    console.error(`Error setting cache for key ${key}`, error);
     return null;
   }
 };
